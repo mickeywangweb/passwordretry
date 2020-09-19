778abd6db268e21b576de21f895c19e902630f5b
Mickey wang <a0958062058@gmail.com>
2020-09-19 12:17:18 +0800

    basic version done

---
+++ passwordretry.py
@@ -0,0 +1,18 @@
+# 密碼重試程式
+# 先在程式碼中 設定好密碼 password = 'A123456'
+# 讓使用者(最多輸入三次密碼)= while
+# 如果輸入正確  就印出"登入成功"
+# 如果不正確 就印出 "密碼錯誤!還有_次機會"
+
+password = 'a123456'
+i = 3 # 剩餘機會
+while True:
+	pwd = input('請輸入密碼')
+	if pwd == password:
+		print('登入成功!')
+		break # 逃出迴圈
+	else:
+		i = i - 1
+		print('密碼錯誤!還有' , i, '次機會')
+		if i == 0:
+			break
